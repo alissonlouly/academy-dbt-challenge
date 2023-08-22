3b8e7d5ffe01b309cdd9413d458d5be702fcce1c
Alisson  Louly <alissonlouly@gmail.com>
2023-08-22 01:40:38 +0000

    fix dim_loc

--- models/marts/dim_loc.sql
+++ models/marts/dim_loc.sql
@@ -10,12 +10,12 @@ with
         from {{ ref('stg_erp__stateprovince') }}
     ),
 
-    erp_territorio as (
+    erp_pais as (
         select *
-        from {{ ref('stg_erp__salesterritory') }}
+        from {{ ref('stg_erp__countryregion') }}
     ),
 
-    join_endereco_estado_territorio as (
+    join_endereco_estado_pais as (
         select
             erp_estado.territorioID,
             erp_endereco.enderecoID,
@@ -24,13 +24,13 @@ with
             erp_estado.nomeEstado,
             erp_endereco.cidade,
             erp_estado.codigoPais,
-            erp_territorio.nomePais,
-            erp_territorio.regiao
+            erp_pais.nomePais,
+           
 
         from erp_endereco
         left join erp_estado on erp_endereco.estadoID = erp_estado.estadoID
-        left join erp_territorio on erp_estado.territorioID = erp_territorio.territorioID
+        left join erp_pais on erp_estado.codigoPais = erp_pais.codigoPais
     )
 
     select *
-    from join_endereco_estado_territorio
+    from join_endereco_estado_pais
